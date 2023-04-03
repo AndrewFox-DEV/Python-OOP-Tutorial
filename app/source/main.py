b22857bb1c1b56bd1c6f0c2489b0b90cc9373297
AndrewFox_DEVELOPER <114300206+AndrewFox-DEV@users.noreply.github.com>
2023-04-03 18:34:19 +0200

    Update main.py

--- app/source/main.py
+++ app/source/main.py
@@ -3,8 +3,7 @@ from Rectangle import Rectangle
 # Example to create an instance/ object through
 # the constructor, which we will use to initialize
 # the class attributes
-rectangle = Rectangle()
-print(rectangle.calculate_area())
+rectangle = Rectangle(1, 100)
 # If the method was static we won't use the instance,
 # but Rectangle.method(args), so the instance will never
 # be created, an example of this is the math module
@@ -12,3 +11,19 @@ print(rectangle.calculate_area())
 # We will use the __str__() method to print the class attributes,
 # but if it weren't there we would see the class instance
 # address in memory
+
+# The del keyword is used to delete objects.
+# In Python everything is an object, so the del
+# keyword can also be used to delete variables,
+# lists, or parts of a list etc.
+del rectangle
+
+# The object that was created earlier we want to delete!
+# So let's use del as a keyword! We use a try-except to
+# check if the object actually exists, which it never will!
+# Without try-except the output would be that the object has
+# not been defined, and therefore has actually been deleted.
+try:
+    print(rectangle)
+except:
+    print('The object has been deleted!')
